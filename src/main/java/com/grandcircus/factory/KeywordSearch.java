cdc2da2ebf9b8cbfd1da4ba68ef88244fa40cfd1
mmerino <pegsescano@yahoo.com>
2015-03-12 16:29:15 -0400

    initial commit

---
+++ src/main/java/com/grandcircus/factory/KeywordSearch.java
@@ -0,0 +1,32 @@
+package com.grandcircus.factory;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+
+public class KeywordSearch {
+	String file = ("resources/SearchTerms.txt");
+
+	public List<String> keyWords(String file) {
+		List<String> keyWords = new ArrayList<String>();
+		
+		ClassLoader classLoader = getClass().getClassLoader();
+		File file3 = new File(classLoader.getResource(file).getFile());
+
+		try {
+			BufferedReader buff = new BufferedReader(new FileReader(file3));
+			for (String str = buff.readLine(); str != null; str = buff.readLine()) {
+				keyWords.add(str);
+			}
+			buff.close();
+		} catch (IOException e) {
+			System.out.println("Exception -- " + e.getMessage());
+			System.exit(1);
+			return null;
+		}
+		return keyWords;
+		
+	}
+}
---
+++ src/test/java/com/grandcircus/factory/AllTests.java
@@ -0,0 +1,11 @@
+package com.grandcircus.factory;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ KeywordSearchTest.class, WebpageSearchTest.class })
+public class AllTests {
+
+}
